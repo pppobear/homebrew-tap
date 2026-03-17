1ebc0b4733b8e3ab849094c8e87da982dbd91855
pppobear <pppobear@gmail.com>
2026-03-17 18:42:37 +0800

    Remove redundant formula version

--- Formula/pinentry-rbw-macos.rb
+++ Formula/pinentry-rbw-macos.rb
@@ -2,7 +2,6 @@ class PinentryRbwMacos < Formula
   desc "macOS pinentry for rbw with Touch ID or Apple Watch authentication"
   homepage "https://github.com/pppobear/pinentry-rbw-macos"
   url "https://github.com/pppobear/pinentry-rbw-macos/archive/refs/tags/v0.1.0.tar.gz"
-  version "0.1.0"
   sha256 "ab3af063fe4e8b695bdf91b9c4460bd34d6967139dfdbb0d46f9349dff207c7b"
   head "https://github.com/pppobear/pinentry-rbw-macos.git", branch: "main"
 
