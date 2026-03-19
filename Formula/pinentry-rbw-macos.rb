437e04480a49ebcd314362d64e38f2bdc682527e
pppobear <pppobear@gmail.com>
2026-03-19 11:18:36 +0800

    Update pinentry-rbw-macos to v0.2.0

--- Formula/pinentry-rbw-macos.rb
+++ Formula/pinentry-rbw-macos.rb
@@ -1,8 +1,8 @@
 class PinentryRbwMacos < Formula
   desc "macOS pinentry for rbw with Touch ID or Apple Watch authentication"
   homepage "https://github.com/pppobear/pinentry-rbw-macos"
-  url "https://github.com/pppobear/pinentry-rbw-macos/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "ab3af063fe4e8b695bdf91b9c4460bd34d6967139dfdbb0d46f9349dff207c7b"
+  url "https://github.com/pppobear/pinentry-rbw-macos/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "4c3611e04c8864e34f9ed191ff2944585f4e350752acf94a8a5af2f675fd5751"
   head "https://github.com/pppobear/pinentry-rbw-macos.git", branch: "main"
 
   depends_on :macos
