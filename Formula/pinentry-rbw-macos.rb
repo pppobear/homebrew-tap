06b0470d7efb65ec91df450084653c8f5e09f5e3
pppobear <pppobear@gmail.com>
2026-07-11 18:34:16 +0800

    Update pinentry-rbw-macos to v0.2.3

--- Formula/pinentry-rbw-macos.rb
+++ Formula/pinentry-rbw-macos.rb
@@ -1,16 +1,16 @@
 class PinentryRbwMacos < Formula
   desc "macOS pinentry for rbw with Touch ID or Apple Watch authentication"
   homepage "https://github.com/pppobear/pinentry-rbw-macos"
-  version "0.2.2"
+  version "0.2.3"
 
   on_arm do
-    url "https://github.com/pppobear/pinentry-rbw-macos/releases/download/v0.2.2/pinentry-rbw-macos-v0.2.2-macos-arm64.zip"
-    sha256 "7567c35ff8e44be085402ba11a686c64016fc9f1b2dc53b26d71bc7bbb82bfc7"
+    url "https://github.com/pppobear/pinentry-rbw-macos/releases/download/v0.2.3/pinentry-rbw-macos-v0.2.3-macos-arm64.zip"
+    sha256 "5b2d9d9a6a32d49bc7686168ae4950792d733414f9e94c3eeab6e165e2892dab"
   end
 
   on_intel do
-    url "https://github.com/pppobear/pinentry-rbw-macos/releases/download/v0.2.2/pinentry-rbw-macos-v0.2.2-macos-x86_64.zip"
-    sha256 "08e6fcb8e769e6381d708ea0e855a8d1866f23be21a250249f6998fc27fd036c"
+    url "https://github.com/pppobear/pinentry-rbw-macos/releases/download/v0.2.3/pinentry-rbw-macos-v0.2.3-macos-x86_64.zip"
+    sha256 "12f2ee0156e8dbb74255cc2a8473f05a8bde2adc92d90570f8a5beb8981897f9"
   end
 
   depends_on macos: :ventura
